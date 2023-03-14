d5c377b89dd7da06279337c497db89f1dfb5e5fa
zakyahk <zakyahk@icloud.com>
2023-03-14 23:20:30 +0700

    Add project files.

---
+++ TPMODUL4_1302210135/KodePos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMODUL4_1302210135;
+
+public class KodePos
+{
+    public string getKodePos(string kelurahan)
+    {
+        switch (kelurahan)
+        {
+            case "Batununggal":
+                return "40266";
+            case "Kujangsari":
+                return "40287";
+            case "Mengger":
+                return "40267";
+            case "Wates":
+                return "40256";
+            case "Cijaura":
+                return "40287";
+            case "Jatisari":
+                return "40286";
+            case "Margasari":
+                return "40286";
+            case "Sekajati":
+                return "40286";
+            case "Kebonwaru":
+                return "40272";
+            case "Maleer":
+                return "40274";
+            case "Samoja":
+                return "40273";
+            default:
+                return "Kode pos tidak ditemukan";
+        }
+    }
+
+    public class MainClass
+    {
+        public static void Main()
+        {
+            KodePos kodepos = new KodePos();
+            string kelurahan = "Batununggal";
+             string kode = kodepos.getKodePos(kelurahan);
+            Console.WriteLine($"Kode pos kelurahan {kelurahan} adalah {kode}");
+        }
+    }
+
+
+}
---
+++ TPMODUL4_1302210135/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class DoorMachine
+{
+    static void Main(string[] args)
+    {
+        DoorMachine doorMachine = new DoorMachine();
+
+        doorMachine.Open(); //Pintu tidak terkunci
+        doorMachine.Close(); //Pintu tertutup
+        doorMachine.Close(); //Pintu sudah tertutup
+        doorMachine.Open(); // Pintu tidak terkunci
+
+    }
+}
+
+public class DoorMachine
+{
+    private DoorState state;
+
+    public DoorMachine()
+    {
+        state = DoorState.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void Open()
+    {
+        if (state == DoorState.Terkunci)
+        {
+            state = DoorState.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else if (state == DoorState.Terbuka)
+        {
+            Console.WriteLine("Pintu sudah terbuka");
+        }
+        else if (state == DoorState.Tertutup)
+        {
+            Console.WriteLine("Pintu masih tertutup");
+        }
+    }
+    public void Close()
+    {
+        if (state == DoorState.Terkunci)
+        {
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+        else if (state == DoorState.Terbuka)
+        {
+            state == DoorState.Tertutup;
+            Console.WriteLine("Pintu tertutup");
+        }
+        else if (state == DoorState.Tertutup)
+        {
+            Console.WriteLine("Pintu sudah tertutup");
+        }
+    }
+}
+
+public enum DoorState
+{
+    Terkunci,
+    Terbuka,
+    Tertutup
+}
